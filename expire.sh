495e2a37e3f09b581f96e034ced3a786a3b9a042
alectramell <alectramell@gmail.com>
2017-08-09 15:01:49 -0700

    expire

---
+++ expire.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+clear
+
+ACTIVE="true"
+
+clear
+
+green=$(tput setaf 2)
+red=$(tput setaf 1)
+reset=$(tput sgr0)
+
+clear
+
+STATUS=$(curl -s https://raw.githubusercontent.com/alectramell/expire/master/status.txt)
+
+clear
+
+EXPIRATION() {
+
+	if [ $STATUS != 1 ]
+	then
+		echo "..${red}EXPIRED${reset}.." | pv -qL 10
+		sleep 30
+		clear
+		bash $(pwd)/expire.sh
+	else
+		echo "..${green}ACTIVE${reset}.." | pv -qL 10
+		sleep 30
+		clear
+		bash $(pwd)/expire.sh
+	fi	
+}
+
+while [ $ACTIVE = "true" ]
+do
+	EXPIRATION
+done
